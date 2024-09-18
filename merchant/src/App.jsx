e3f55bd8144e798768955a90622669f3a2115986
RohanGorla <rohangorla2010@gmail.com>
2024-09-18 17:37:04 +0530

    modify category and company select inputs to select options tag

--- merchant/src/App.jsx
+++ merchant/src/App.jsx
@@ -3,37 +3,37 @@ import axios from "axios";
 import "./App.css";
 
 function App() {
-  const [file, setFile] = useState();
-  const [name, setName] = useState("");
+  const [file, setFile] = useState("");
+  const [name, setName] = useState("Product ");
   const [cat, setCat] = useState("");
   const [company, setCompany] = useState("");
-  const [mrp, setMrp] = useState("");
-  const [price, setPrice] = useState("");
+  const [mrp, setMrp] = useState(45000);
+  const [price, setPrice] = useState(35000);
   const [url, setUrl] = useState("");
 
   async function addProduct() {
     let response = await axios.post("http://localhost:8008/addProduct", {
-      fileType: file.type,
+      // fileType: file.type,
       name: name,
       cat: cat,
       company: company,
       mrp: mrp,
       price: price,
-      url: url,
+      // url: url,
     });
     console.log(response.data);
-    setFile();
-    setName("");
-    setCat("");
-    setCompany("");
-    setMrp("");
-    setPrice("");
-    setUrl("");
-    let res2 = await axios.put(response.data, file, {
-      headers: {
-        "Content-Type": file.type,
-      },
-    });
+    // setFile();
+    // setName("");
+    // setCat("");
+    // setCompany("");
+    // setMrp("");
+    // setPrice("");
+    // setUrl("");
+    // let res2 = await axios.put(response.data, file, {
+    //   headers: {
+    //     "Content-Type": file.type,
+    //   },
+    // });
   }
 
   return (
@@ -59,24 +59,58 @@ function App() {
         ></input>
       </div>
       <div className="Category">
-        <input
+        {/* <input
           type="text"
           value={cat}
           onChange={(e) => {
             setCat(e.target.value);
           }}
           placeholder="Enter category"
-        ></input>
+        ></input> */}
+        <select
+          onChange={(e) => {
+            setCat(e.target.value);
+          }}
+        >
+          <option defaultValue={true} hidden>
+            Select category
+          </option>
+          <option value="Smartphone">Smartphone</option>
+          <option value="Laptop">Laptops & Tabs</option>
+          <option value="Television">Televisions</option>
+          <option value="Kitchen">Kitchen appliances</option>
+        </select>
       </div>
       <div className="Company">
-        <input
+        {/* <input
           type="text"
           value={company}
           onChange={(e) => {
             setCompany(e.target.value);
           }}
           placeholder="Enter company name"
-        ></input>
+        ></input> */}
+        <select
+          onChange={(e) => {
+            setCompany(e.target.value);
+          }}
+        >
+          <option defaultValue={true} hidden>
+            Select company
+          </option>
+          <option value="Samsung">Samsung</option>
+          <option value="Xiaomi">Xiaomi</option>
+          <option value="Oneplus">Oneplus</option>
+          <option value="Vivo">Vivo</option>
+          <option value="Realme">Realme</option>
+          <option value="LG">LG</option>
+          <option value="IQOO">IQOO</option>
+          <option value="Godrej">Godrej</option>
+          <option value="Daikin">Daikin</option>
+          <option value="ASUS">ASUS</option>
+          <option value="Apple">Apple</option>
+          <option value="Nokia">Nokia</option>
+        </select>
       </div>
       <div className="MRP">
         <input
